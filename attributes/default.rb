0405c80522dd42f13d9a6da9a74d6f18d1a0bbf8
pleycpl <h4rvey@protonmail.com>
2018-09-28 15:58:27 +0300

    Clone cesi repository, create cesi user and group

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+# Defaults
+default['cesi']['user'] = 'cesi'
+default['cesi']['group'] = 'cesi'
+default['cesi']['setup_path'] = '/opt/cesi'
+default['cesi']['git']['repository'] = 'https://github.com/gamegos/cesi.git'
+default['cesi']['git']['revision'] = 'master'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,28 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 include_recipe 'git::default'
+
+cesi_user = node['cesi']['user']
+cesi_group = node['cesi']['group']
+cesi_setup_path = node['cesi']['setup_path']
+
+group cesi_group
+
+user cesi_user do
+    manage_home false
+    shell '/bin/nologin'
+    gid cesi_group
+    home cesi_setup_path
+end
+
+directory cesi_setup_path do
+    user cesi_user
+    group cesi_group
+    recursive true
+end
+
+git cesi_setup_path do
+    repository node['cesi']['git']['repository']
+    revision node['cesi']['git']['revision']
+    action :sync
+end
